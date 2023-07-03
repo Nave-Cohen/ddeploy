931cdca6a2f28f7a8711cdab1a5154c704f17d99
nave <nave1616@hotmail.com>
2023-07-03 20:35:11 +0300

    make Dockerfile clone git repo

--- app/entities/user.js
+++
@@ -1,9 +0,0 @@
-class User {
-  constructor(user) {
-    this.id = user.id;
-    this.name = user.username;
-    this.email = user.email;
-    this.letter = user.username.charAt(0);
-  }
-}
-module.exports = User;
