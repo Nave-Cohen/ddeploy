d5166113fbae33c08859d947d2d81033dfe4c990
nave <nave1616@hotmail.com>
2023-07-04 14:16:33 +0300

    acme-challenge

--- certbot-entrypoint.sh
+++ certbot-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # Run Certbot with custom directory paths
-certbot certonly --standalone --preferred-challenges http --email nave1616@hotmail.com --agree-tos -d nave.autos --non-interactive --keep-until-expiring
+certbot certonly --webroot -w /var/www/letsencrypt --preferred-challenges http-01 --email nave1616@hotmail.com --agree-tos -d nave.autos --non-interactive --keep-until-expiring
 cp -R /etc/letsencrypt/. /certs
+cp -R /var/www/letsencrypt/. /html
