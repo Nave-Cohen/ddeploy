64307449343257803ea373ff65bb7e06d659cf4d
Nave Cohen <nave.cohen@e.braude.ac.il>
2023-07-10 11:08:05 +0300

    run cleaner before init

--- ddeploy/etc/ddeploy/commands/init/index.sh
+++ ddeploy/etc/ddeploy/commands/init/index.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 # Check if the current directory is already a runner environment
-folder=$(/usr/bin/env bash "$base/helpers/isWorkdir.sh")
+folder=$("$base/helpers/isWorkdir.sh")
 if [[ -n $folder ]]; then
     echo "$(basename "$folder") is Already a runner environment"
     exit 1
 fi
+
+#Run cleaner to clean deploment.json if there is entry
+$base/maintence/cleaner.sh
+
 # Check if the required arguments are provided
 if [ $# -lt 2 ]; then
     echo "[ERROR] - doc init [github url] [branch name]"
@@ -28,23 +32,24 @@ echo + docker-compose.yml
 echo + app/Dockerfile
 echo + entrypoint
 
-# Check if .env file exists
-if [[ ! -f ".env" ]]; then
-    touch .env
-    echo "+ .env file created."
-fi
 
 # Create .ddeploy.env file
-echo -e "GIT=$1\nBRANCH=$2\nDOMAIN=#example.com\nMAIL=\nBACKEND_IP=\nBACKEND_PORT=3000\n" > .ddeploy.env
-echo "+ .ddeploy.env"
-echo ".ddeploy.env must be edited."
+if [[ ! -f ".ddeploy.env" ]]; then
+    echo -e "GIT=$1\nBRANCH=$2\nDOMAIN=#example.com\nMAIL=\nBACKEND_IP=\nBACKEND_PORT=3000\nMYSQL_USER=#USERNAME\nMYSQL_PASSWORD=#PASSWORD\nMYSQL_DATABASE=#DB_NAME\n" > .ddeploy.env
+    echo "+ .ddeploy.env"
+    echo ".ddeploy.env must be edited."
+fi
 
 export $(grep -v '^#' ".ddeploy.env" | xargs)
+
 # Extract repository and commit information
-last_commit=$(/usr/bin/env bash $base/helpers/fetchCommit.sh)
+last_commit=$($base/helpers/fetchCommit.sh)
 
 # Update the JSON file with the new item
 json_file="$base/configs/deploys.json"
 new_item=$(jq -n --arg folder "$PWD" --arg commit "$last_commit" '{"folder": $folder, "commit": $commit}')
 jq --argjson new_item "$new_item" '. + [$new_item]' "$json_file" > "$json_file.tmp" && \
 mv "$json_file.tmp" "$json_file"
+
+# Print success message
+echo "Runner environment initialized successfully."
