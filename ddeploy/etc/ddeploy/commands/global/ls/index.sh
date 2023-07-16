6a61a6ab24d067d9c716d856a3c8785a0b81c6f2
Nave Cohen <nave.cohen@e.braude.ac.il>
2023-07-16 21:04:39 +0300

    addd ls command

--- ddeploy/etc/ddeploy/commands/global/ls/index.sh
+++ ddeploy/etc/ddeploy/commands/global/ls/index.sh
@@ -3,7 +3,7 @@
 source /etc/ddeploy/helpers/status.sh
 source /etc/ddeploy/helpers/json.sh
 
-printf "%-10s %-20s %-20s %-20s %-15s %-20s\n" "Project" "HTTP Status" "Docker Status" "Last build" "Branch" "Commit"
+printf "%-20s %-20s %-20s %-20s %-15s %-20s\n" "Project" "HTTP Status" "Docker Status" "Last build" "Branch" "Commit"
 
 folders=$(getAll "folder")
 read -a folders < <(getAll "folder")
